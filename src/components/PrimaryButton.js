1f2b6c6846a8693c29fed25cacebe2db3a93be38
watsontrevor10 <watsontrevor10@gmail.com>
2020-03-11 21:40:57 -0600

    made some styling edit to buttons

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -2,8 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const PrimaryButton = styled.button`
-    border-radius: 8px;
-    text-color: white;
+  border-radius: 8px;
+  border: none;
+  color: white;
+  background-color: #0082bf;
 `
 
 export default PrimaryButton
