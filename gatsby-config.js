99a7cda6e41eaa08ef8f6381ca081e182af32582
Trevor Watson <trevorwatson@Mac-mini.local>
2020-08-22 18:47:10 -0600

    added fathom analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-fathom',
+      options: {
+        siteId: 'XXDMIDYO'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
